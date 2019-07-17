e12b8b31f72a93014e2d8f866f1f2b53c230c7d3
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 21:13:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+#def oxford_comma(array)
+  #  if array.length == 2
+  #  array.join(" and ")
+  #elsif array.length > 2
+  #  array.insert(-0, "and ")
+  #  array.join(", ")
+  #end
+#oxford_comma
+#end
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
